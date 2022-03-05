4d47540803a61829ecaf9879e9d3fa03ae1b5ede
Daniel Mendez <awilddanoru@gmail.com>
2022-03-05 11:25:09 -0800

    Add animation for chosen colors by both CPU and player

--- game.js
+++ game.js
@@ -1,13 +1,31 @@
-let gamePattern = [];
-let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = []; // This is the pattern the CPU picks for the game using the nextSequence function.
+
+let userClickedPattern = []; // This is the pattern of play from the user based on their clicks.
+
+const buttonColors = ["red", "blue", "green", "yellow"]; // This is the array of the four colors being used in the game.
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
   $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-  let soundColor = new Audio("sounds/" + randomChosenColor +".mp3");
-  soundColor.play();
+  let cpuSound = new Audio("sounds/" + randomChosenColor +".mp3");
+  cpuSound.play();
   console.log(gamePattern);
-}
+};
 
+$(".btn").click(function() {
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);  
+  let playerSound = new Audio("sounds/" + userChosenColor +".mp3");
+  playerSound.play();
+  animatePress(userChosenColor);
+  console.log(userChosenColor);
+});
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function(){
+    $("#"+ currentColor).removeClass("pressed");
+  }, 100)
+};
