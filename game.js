5e6ddfd3ebf65d4010d5ca2afbe7e763062d5862
Daniel Mendez <awilddanoru@gmail.com>
2022-03-05 08:47:37 -0800

    Added sound and flash functionality to game

--- game.js
+++ game.js
@@ -2,9 +2,12 @@ let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  let soundColor = new Audio("sounds/" + randomChosenColor +".mp3");
+  soundColor.play();
   console.log(gamePattern);
 }
 
